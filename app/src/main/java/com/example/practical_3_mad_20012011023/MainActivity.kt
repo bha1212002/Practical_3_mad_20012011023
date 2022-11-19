420c36b38eb4d16e694ce6880511dd4fd9668b47
RS <bhvaikchhadriagmail.com>
2022-11-20 00:36:13 +0530

    Practical_3_mad_20012011023-master

---
+++ app/src/main/java/com/example/practical_3_mad_20012011023/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.practical_3_mad_20012011023
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practical_3_mad_20012011023.R
+import com.example.practical_3_mad_20012011023.databinding.ActivityMainBinding
+//import com.example.practical_3_mad.R
+//import com.example.practical_3_mad_20012011023.databinding.ActivityMainBinding
+//import com.example.practical_3_mad_20012011023.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+//        val signup = findViewById<TextView>(R.id.signup)
+
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_login
+        binding.bottomNavigationView.setOnItemSelectedListener { it1 ->
+            when(it1.itemId){
+                R.id.bottom_nav_reg -> {
+                    Intent(this,Registration::class.java).also{startActivity(it)}
+                }
+
+            }
+            return@setOnItemSelectedListener true
+        }
+
+
+
+    }
+}
